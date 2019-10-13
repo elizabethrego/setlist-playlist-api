81655a256b1e73249a2f258ac1d8e945046a4e60
Elizabeth Rego <elizabethmrego@gmail.com>
2019-10-12 22:54:00 -0700

    big update. huge! still no actual playlist.

--- src/createPlaylist.js
+++ src/createPlaylist.js
@@ -1,202 +1,183 @@
-'use strict';
+'use strict'
 
-const https = require('https');
-const promise = require('promise');
+const https = require('https')
+const promise = require('promise')
 
-const spotifyURLRoot = process.env.SPOTIFY_API_DOMAIN;
-const searchTrackEndpoint = process.env.SPOTIFY_API_SEARCH_TRACK_ENDPOINT;
-
-const noMatchMessage = process.env.STRING_NO_SETLIST_MATCH;
-const errorMessage = process.env.STRING_ERROR;
-
-let options, token;
-let setlistInfo = {};
-
-module.exports.handler = (event, context, callback) => {
-    console.log('Request:' + JSON.stringify(event));
-
-    // Find each song in setlist and create Spotify  playlist
-    createPlaylist(event)
-    .then( (result) => { doCallback( { success: true, message: result }, event, callback) })
-    .catch( (error) => { doCallback( { success: false, message: error }, event, callback) });
-};
-
-function createPlaylist(event) {
-  return new Promise((resolve, reject) => {
-
-    if (event.body && event.body.message) {
-      let body = event.body.message;
-
-      if (body.token && body.setlist && body.artistName) {
-        token = body.token;
-
-        options = {
-            headers: {
-                'Authorization': 'Bearer ' + token
-            }
-        };
-
-        setlistInfo.setlist = body.setlist;
-        setlistInfo.artist = body.artistName;
-
-        processEachSong()
-        .then( (trackList) => { return processTrackList(trackList); })
-        .then( (result) => { return createPlaylistOnSpotify(result); })
-        .then( (uris) => { resolve(uris); })
-        .catch( (error) => { reject(error); });
-      } else {
-        reject(errorMessage);
-      }
-    }
-  });
+const processEvent = (eventToProcess) => {
+  if (eventToProcess.body && eventToProcess.body.message) {
+    let message = eventToProcess.body.message
+     
+    if (message.token  && message.artistName && message.songs) return message
+    else return false
+  }
 }
 
-function processEachSong() {
+const createPlaylist = (setlistInfo) => {
   return new Promise((resolve, reject) => {
-    let trackList = [];
-    let trackListPromises = [];
-    
-    setlistInfo.setlist.forEach( (item, index) => {
-      trackListPromises[index] = new Promise( (innerResolve) => {
-        searchForTrack(item.name).then( (trackUri) => {
-          trackList[index] = trackUri;
-          return innerResolve(trackUri);
-        }).catch( (error) => { reject(error); });
-      });
-    });
+    if (setlistInfo) {
+      let mySetlistInfo = new SetlistInfo(setlistInfo.token, setlistInfo.artistName, setlistInfo.songs)
 
-    Promise.all(trackListPromises)
-    .then( () => { resolve(trackList); });
-
-  });
+      mySetlistInfo.stageSongsForPlaylist()
+        .then( (trackUris) => {
+          if (trackUris) return mySetlistInfo.processTrackUris(trackUris)  
+          else reject(process.env.STRING_ERROR)
+        }) 
+        .then( (result) => createPlaylistOnSpotify(result) )
+        .then( (urisForSpotify) => resolve(urisForSpotify) )
+        .catch( (error) => reject(error) )
+    } else reject(process.env.STRING_ERROR)
+  })
 }
 
-function searchForTrack(track) {
-  return new Promise((resolve, reject) => {
-        let query = formatSearchQuery(track);
-
-        doRequest(spotifyURLRoot + searchTrackEndpoint + query, options)
-        .then(function(result) { return processSearchResult(track, result); })
-        .then(function (matchingUri) { resolve(matchingUri); }) // return the matching URI, or nada
-        .catch(function(error) { console.log('catching'); reject(error); });
-    });
-}
-
-function processSearchResult(songName, searchResult) {
-  return new Promise((resolve, reject) => {
-    if (searchResult && JSON.parse(searchResult).tracks) {
-        let potentialMatches = JSON.parse(searchResult).tracks.items;
-
-        if (potentialMatches.length) {  
-          potentialMatches.forEach( (item, index) => {
-              if (checkSongNameMatch(songName, item.name) && item.uri && item.name) {
-                  resolve(item.uri);
-              } else {
-                if (index == potentialMatches.length) {
-                  // TODO: Check for cover
-                  console.log("No match for track \"" + songName + "\".");
-                  resolve();
-                }
-              }
-          });
-        } else {
-          console.log("No match for track \"" + songName + "\".");
-          resolve();
-        }
-      }
-  });
-}
-
-function processTrackList(trackList) {
-  return new Promise((resolve, reject) => {
-    let uris = '';
-
-    let missingSongs = [];
-
-    for (let i = 0; i < setlistInfo.setlist.length; i++) {
-      if (trackList[i]) {
-        uris += trackList[i];
-        if (i < setlistInfo.setlist.length - 1) {
-          uris += ',';
-        }
-      } else {
-        missingSongs.push(setlistInfo.setlist[i].name);
-      }
-    }
-
-    if (missingSongs.length < setlistInfo.setlist.length) {
-      resolve({
-        uris: uris,
-        missingSongs: missingSongs
-      });
-    } else {
-      reject("No tracks found.");
-    }
-  });
-}
-
-function createPlaylistOnSpotify(result) {
+const createPlaylistOnSpotify = (result) => {
   return new Promise((resolve, reject) => {
     // just passing it back for now, should do something like create a playlist
-    resolve(result.uris);
-  });
+    resolve(result.uris)
+  })
 }
 
-function checkSongNameMatch(songNameToMatch, potentialMatch) {
-  return potentialMatch.toLowerCase() == songNameToMatch.toLowerCase();
+const checkSongNameMatch = (firstName = '', secondName = '') => {
+  return firstName.toLowerCase() == secondName.toLowerCase()
 }
 
-function formatName(name) {
-    let splitName = name.split(' ');
-    let formattedName = '';
-
-    for (let i = 0; i < splitName.length; i++) {
-        formattedName += splitName[i];
-
-        if (i < splitName.length - 1) {
-            formattedName += '+';
-        }
-    }
-    return formattedName;
+const formatArtistOrSongName = (name = '') => {
+  let splitName = name.split(' ')
+  let formattedName = ''
+  let numNamePieces = splitName.length
+  
+  for (let i = 0; i < numNamePieces; i++) {
+      formattedName += splitName[i]
+      formattedName += i < numNamePieces - 1  ? '+' :  '' // can we comment out last part?
+  }
+  
+  return formattedName
 }
 
-function formatSearchQuery(track) {
-    return '?q=' + formatName(track) + '+' + formatName(setlistInfo.artist) + '&type=track';
-}
-
-function doRequest(url, options) {
+const doRequest = (url, options) => {
   return new Promise((resolve, reject) => {
-
     https.get(url, options, (res) => {
-      // console.log('Status Code:' + res.statusCode);
-      // console.log('Headers:' + JSON.stringify(res.headers));
+      res.setEncoding('utf8')
 
-      res.setEncoding('utf8');
+      let responseData = ''
 
-      let responseData = '';
-      res.on('data', (d) => { responseData += d; });
+      res.on('data', (d) => { responseData += d } )
 
       res.on('end', (e) => {
-        let potentialError = JSON.parse(responseData).error;
+        let potentialError = JSON.parse(responseData).error
 
-        if (potentialError) { reject(potentialError.message); }
-        else { resolve(responseData); }
-      });
+        if (potentialError) {
+          reject(potentialError.message)
+        }
+        else resolve(responseData)
+      })
 
-      res.on('error', (e) => { reject(e); });
-    });
-  });
+      res.on('error', (e) => reject(e) )
+    })
+  })
 }
 
-function doCallback(m, e, cb) {
-  cb(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: m,
-      input: e,
-    },
-    null,
-    2),
-  });
+class SetlistInfo {
+  constructor(token = '', artist = '', songs = []) {
+    this.httpOptions = {
+      headers: {
+          'Authorization': `Bearer ${token}`
+      }
+    }
+    
+    this.artist = artist
+    this.songs = songs
+    this.missingSongs = []
+    
+    this.strings = {
+      spotifyURLRoot: process.env.SPOTIFY_API_DOMAIN,
+      searchTrackEndpoint: process.env.SPOTIFY_API_SEARCH_TRACK_ENDPOINT
+    }
+  }
+  
+  stageSongsForPlaylist() {
+    return new Promise((resolve, reject) => {
+      let songPromises = []
+      let trackUris = []
+      
+      // Search for each song on Spotify, add the matching URI to returned list
+      this.songs.forEach( (item, index) => {
+        songPromises[index] = new Promise( (innerResolve) => { 
+          this.searchForSong(item.name)
+          .then( (trackUri) => {
+            trackUris[index] = trackUri
+            return innerResolve(trackUri)
+          }).catch( (error) => {
+            if (error.substring('token')) reject(error)
+            else resolve(error) 
+          })
+        })
+      }) 
+  
+      Promise.all(songPromises)
+      .then( () => resolve(trackUris) )
+    })
+  }
+  
+  searchForSong(song) {
+    return new Promise((resolve, reject) => {
+      let query = this.formatSearchQuery(song)
+
+      doRequest(this.strings.spotifyURLRoot + this.strings.searchTrackEndpoint + query, this.httpOptions)
+      .then( (searchResult) => this.findMatchingTrackUri(song, searchResult) ) 
+      .then(  (matchingUri) => resolve(matchingUri) ) // return the matching URI, or nada
+      .catch( (error) => reject(error) ) // will cause the whole playlist to fail
+    })
+  }
+  
+  formatSearchQuery(track =  '') {
+    return `?q=${formatArtistOrSongName(track)}+${formatArtistOrSongName(this.artist)}&type=track`
+  } 
+  
+  findMatchingTrackUri(songName = '', searchResult = '{tracks:{items:{}}}') {
+    return new Promise((resolve, reject) => {
+      let potentialMatches = JSON.parse(searchResult).tracks.items
+      let foundMatch = false
+      
+      if (potentialMatches.length) {
+        for (let i = 0; i < potentialMatches.length; i++) {
+          if (!foundMatch  && potentialMatches[i].name && potentialMatches[i].uri && checkSongNameMatch(songName, potentialMatches[i].name)) {
+            foundMatch = true
+            return resolve(potentialMatches[i].uri)
+          }  else if (i == potentialMatches.length - 1) resolve()
+        } 
+      } else resolve() 
+    })
+  }
+  
+  processTrackUris(trackUris) {
+    return new Promise((resolve, reject) => {
+      let uriString = ''
+      let numSongs = this.songs.length
+  
+      for (let i = 0; i < numSongs; i++) {
+        if (trackUris[i]) {
+          uriString += trackUris[i]
+          uriString += i < numSongs - 1 ? ',' : ''
+        } else this.missingSongs.push(this.songs[i].name) 
+      }
+  
+      if (this.missingSongs.length < this.songs.length) {
+        resolve({
+          uris: uriString,
+          missingSongs: this.missingSongs
+        })
+      } else reject(process.env.STRING_ERROR)
+    })
+  }
+  
 }
 
+module.exports.handler = (event, context, callback) => {
+    let processedEvent = processEvent(event)
+    
+    // Find each song in setlist and create Spotify  playlist
+    createPlaylist(processedEvent)
+    .then( (result) => callback(null, result) )
+    .catch( (error) => {
+      return callback(error) }) 
+}
