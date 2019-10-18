830a4a98f2aac30db1d28823d18025df42102ed1
Elizabeth Rego <elizabethmrego@gmail.com>
2019-10-17 19:04:35 -0700

    she works

--- src/createPlaylist.js
+++ src/createPlaylist.js
@@ -11,7 +11,7 @@ const processEvent = (eventToProcess) => {
   }
 }
 
-const createPlaylist = (setlistInfo) => {
+const createPlaylistFromSetlist = (setlistInfo) => {
   return new Promise((resolve, reject) => {
     if (setlistInfo) {
       let mySetlistInfo = new SetlistInfo(setlistInfo.token, setlistInfo.artistName, setlistInfo.songs)
@@ -21,28 +21,12 @@ const createPlaylist = (setlistInfo) => {
           if (trackUris) return mySetlistInfo.processTrackUris(trackUris)  
           else reject(process.env.STRING_ERROR)
         }) 
-        // .then( (result) => createPlaylistOnSpotify(result) )
-        .then( (urisForSpotify) => resolve(urisForSpotify) )
+        .then( (processedTrackUris) => mySetlistInfo.createPlaylistOnSpotify(processedTrackUris) )
+        .then( (playlistResult) => resolve(playlistResult) )
         .catch( (error) => reject(error) )
-    } else reject(process.env.STRING_ERROR)
+    } else reject(process.env.STRING_ERROR) // elli
   })
 }
-
-/*
-const createPlaylistOnSpotify = (result) => {
-  return new Promise((resolve, reject) => {
-    let spotifyURLRoot = process.env.SPOTIFY_API_DOMAIN
-    let createPlaylistEndpoint = process.env.SPOTIFY_API_CREATE_PLAYLIST_ENDPOINT
-
-    doRequest(this.strings.spotifyURLRoot + createPlaylistEndpoint + query, this.httpOptions)
-      .then( (searchResult) => this.findMatchingTrackUri(song, searchResult) ) 
-      .then(  (matchingUri) => resolve(matchingUri) ) // return the matching URI, or nada
-      .catch( (error) => reject(error) ) // will cause the whole playlist to fail
-
-    resolve(result.uris)
-  })
-}
-*/
 
 const checkSongNameMatch = (firstName = '', secondName = '') => {
   return firstName.toLowerCase() == secondName.toLowerCase()
@@ -61,7 +45,7 @@ const formatArtistOrSongName = (name = '') => {
   return formattedName
 }
 
-const doRequest = (url, options) => {
+const doGetRequest = (url, options) => {
   return new Promise((resolve, reject) => {
     https.get(url, options, (res) => {
       res.setEncoding('utf8')
@@ -84,17 +68,64 @@ const doRequest = (url, options) => {
   })
 }
 
+const doPostRequest = (url, options, body) => {
+  return new Promise((resolve, reject) => {
+    let responseData = ''
+    
+   let req = https.request(url, options, (res) => {
+      res.on('data', (d) => responseData += d )
+      
+      res.on('end', () => resolve(responseData) )
+    })
+    
+    req.on('error', (e) => reject(e) )
+    
+    req.write(body);
+    req.end();
+    
+  })
+}
+
 class SetlistInfo {
   constructor(token = '', artist = '', songs = []) {
+    this.authHeader =  `Bearer ${token}`
+    
     this.httpOptions = {
-      headers: {
-          'Authorization': `Bearer ${token}`
+      getOptions: {
+        headers: {
+          'Authorization': this.authHeader 
+        }
+      },
+      postOptions: {
+        method: 'POST',
+        headers: {
+          'Authorization': this.authHeader,
+          'Content-Type': 'application/json'
+        }
       }
     }
     
     this.artist = artist
     this.songs = songs
+    this.trackUris = []
     this.missingSongs = []
+    this.userId = ''
+    this.playlistTitle = `${this.artist}'s Latest Setlist #SetlistToPlaylist`
+    
+    this.strings = {
+      spotifyURLRoot: process.env.SPOTIFY_API_DOMAIN,
+      profileEndpoint: process.env.SPOTIFY_API_PROFILE_ENDPOINT,
+      createPlaylistEndpoint: process.env.SPOTIFY_API_CREATE_PLAYLIST_ENDPOINT,
+      errorMessage: process.env.STRING_ERROR
+    } 
+  }
+  
+  setTrackUris(trackUris) {
+    this.trackUris = trackUris.uris
+  }
+  
+  setUserId(userId) {
+    this.userId = encodeURIComponent(userId)
   }
   
   stageSongsForPlaylist() {
@@ -110,7 +141,8 @@ class SetlistInfo {
             trackUris[index] = trackUri
             return innerResolve(trackUri)
           }).catch( (error) => {
-            if (error.substring('token')) reject(error)
+            if (error instanceof String === false 
+            || error.substring('token')) reject(error)
             else resolve(error) 
           })
         })
@@ -127,7 +159,7 @@ class SetlistInfo {
       let spotifyURLRoot = process.env.SPOTIFY_API_DOMAIN
       let searchTrackEndpoint = process.env.SPOTIFY_API_SEARCH_TRACK_ENDPOINT
 
-      doRequest(spotifyURLRoot + searchTrackEndpoint + query, this.httpOptions)
+      doGetRequest(spotifyURLRoot + searchTrackEndpoint + query, this.httpOptions.getOptions)
       .then( (searchResult) => this.findMatchingTrackUri(song, searchResult) ) 
       .then(  (matchingUri) => resolve(matchingUri) ) // return the matching URI, or nada
       .catch( (error) => reject(error) ) // will cause the whole playlist to fail
@@ -138,7 +170,7 @@ class SetlistInfo {
     return `?q=${formatArtistOrSongName(track)}+${formatArtistOrSongName(this.artist)}&type=track`
   } 
   
-  findMatchingTrackUri(songName = '', searchResult = '{tracks:{items:{}}}') {
+  findMatchingTrackUri(songName = '', searchResult) {
     return new Promise((resolve, reject) => {
       let potentialMatches = JSON.parse(searchResult).tracks.items
       let foundMatch = false
@@ -156,32 +188,76 @@ class SetlistInfo {
   
   processTrackUris(trackUris) {
     return new Promise((resolve, reject) => {
-      let uriString = ''
+      let processedTrackUris = []
       let numSongs = this.songs.length
   
       for (let i = 0; i < numSongs; i++) {
-        if (trackUris[i]) {
-          uriString += trackUris[i]
-          uriString += i < numSongs - 1 ? ',' : ''
-        } else this.missingSongs.push(this.songs[i].name) 
+        if (trackUris[i]) processedTrackUris.push(trackUris[i])
+        else this.missingSongs.push(this.songs[i].name) 
       }
   
       if (this.missingSongs.length < this.songs.length) {
         resolve({
-          uris: uriString,
+          uris: processedTrackUris,
           missingSongs: this.missingSongs
         })
-      } else reject(process.env.STRING_ERROR)
+      } else reject(this.strings.errorMessage)
     })
   }
   
+  getSpotifyUserId() {
+    return new Promise((resolve, reject) => {
+      doGetRequest(this.strings.spotifyURLRoot + this.strings.profileEndpoint, this.httpOptions.getOptions) 
+      .then( (profile) => {
+        this.setUserId(this.getUserIdFromProfile(profile))
+        return resolve(this.userId) 
+      }).catch( (error => reject(this.strings.errorMessage)) )
+    })
+  }
+  
+  createPlaylistOnSpotify(trackUris) {
+    return new Promise((resolve, reject) => {
+      this.setTrackUris(trackUris)
+      
+      this.getSpotifyUserId()
+      .then( (userId) => this.createPlaylist(userId) )
+      .then( (playlistInfo) => this.addSongsToPlaylist(JSON.parse(playlistInfo).id) )
+       .then( (result) => resolve(result) )
+      .catch( (error) => reject(`error: ${error}`) )
+    })
+  }
+  
+  createPlaylist(userId) {
+    return new Promise((resolve, reject) => {
+      let url = `${this.strings.spotifyURLRoot}/v1/users/${userId}/playlists` 
+      let playlistJsonString = JSON.stringify({ name: this.playlistTitle })
+      doPostRequest(url, this.httpOptions.postOptions, playlistJsonString)
+      .then( (playlistId) => resolve(playlistId) )
+      .catch( (error) => reject(`error: ${error}`) )
+    })
+  }
+  
+  addSongsToPlaylist(playlistId) {
+    return new Promise((resolve, reject) => {
+      let url = `${this.strings.spotifyURLRoot}/v1/playlists/${playlistId}/tracks`
+      let trackUrisString = JSON.stringify({ uris: this.trackUris })
+      doPostRequest(url, this.httpOptions.postOptions, trackUrisString)
+      .then( (result) => resolve(result) )
+      .catch( (error) => reject(`error: ${error}`) )
+    })
+  }
+  
+  getUserIdFromProfile(profile) { 
+    let userId = JSON.parse(profile).id
+    if (userId) return(userId)
+  }
 }
 
 module.exports.handler = (event, context, callback) => {
     let processedEvent = processEvent(event)
     
     // Find each song in setlist and create Spotify  playlist
-    createPlaylist(processedEvent)
+    createPlaylistFromSetlist(processedEvent)
     .then( (result) => callback(null, result) )
     .catch( (error) => {
       return callback(error) }) 
